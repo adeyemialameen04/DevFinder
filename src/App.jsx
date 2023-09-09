5f0ce1a304e96f9336e36ce06e00a4c568edf381
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-09-09 23:07:49 +0100

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import Navbar from "./components/navbar/navbar";
 
 function App() {
-
   return (
-  <>
-  Hello
-  </>
+    <>
+      Hello
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="container nav__container">
+        <div className="logo">devfinder</div>
+        <div className="theme">Light</div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
