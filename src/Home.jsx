4b07daebe60b2f04b63004905f9609b0274cf81e
Al Ameen Adeyemi <adeyemialameen04@gmail.com>
2023-09-10 17:15:05 +0100

    fetching data

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import useFetchData from "./utils/useFetchData";
+import { useEffect } from "react";
+import Header from "./components/header/Header";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { data, isLoading, isError, error } = useFetchData("adeyemialameen04");
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default Home;
